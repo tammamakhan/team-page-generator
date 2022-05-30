487cabc0f46b0c9dae8542ba02ac7a0e70730af7
Tammam Khan <tammam.a.khan@gmail.com>
2022-05-30 14:22:28 -0700

    added engineer class and tests, updated manager class and tests

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,39 @@
+const Engineer = require("../lib/Engineer");
+
+test("creates a engineer object", () => {
+  const engineer = new Engineer("John", 1, "john@gmail.com", "johngithub");
+
+  expect(engineer.name).toBe("John");
+  expect(engineer.id).toBe(1);
+  expect(engineer.email).toBe("john@gmail.com");
+});
+
+test("gets a engineer's name", () => {
+  const engineer = new Engineer("John", 1, "john@gmail.com", "johngithub");
+
+  expect(engineer.getName()).toBe("John");
+});
+
+test("gets a engineer's id", () => {
+  const engineer = new Engineer("John", 1, "john@gmail.com", "johngithub");
+
+  expect(engineer.getId()).toBe(1);
+});
+
+test("gets a engineer's email", () => {
+  const engineer = new Engineer("John", 1, "john@gmail.com", "johngithub");
+
+  expect(engineer.getEmail()).toBe("john@gmail.com");
+});
+
+test("gets an engineer's github", () => {
+  const engineer = new Engineer("John", 1, "john@gmail.com", "johngithub");
+
+  expect(engineer.getGithub()).toBe("johngithub");
+});
+
+test("gets a engineer's role", () => {
+  const engineer = new Engineer("John", 1, "john@gmail.com");
+
+  expect(engineer.getRole()).toBe("Engineer");
+});
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -9,25 +9,31 @@ test("creates a manager object", () => {
 });
 
 test("gets a manager's name", () => {
-  const manager = new Manager("John", 1, "john@gmail.com");
+  const manager = new Manager("John", 1, "john@gmail.com", 22);
 
   expect(manager.getName()).toBe("John");
 });
 
 test("gets a manager's id", () => {
-  const manager = new Manager("John", 1, "john@gmail.com");
+  const manager = new Manager("John", 1, "john@gmail.com", 22);
 
   expect(manager.getId()).toBe(1);
 });
 
 test("gets a manager's email", () => {
-  const manager = new Manager("John", 1, "john@gmail.com");
+  const manager = new Manager("John", 1, "john@gmail.com", 22);
 
   expect(manager.getEmail()).toBe("john@gmail.com");
 });
 
+test("gets a manager's office number", () => {
+  const manager = new Manager("John", 1, "john@gmail.com", 22);
+
+  expect(manager.getOfficeNumber()).toBe(22);
+});
+
 test("gets a manager's role", () => {
-  const manager = new Manager("John", 1, "john@gmail.com");
+  const manager = new Manager("John", 1, "john@gmail.com", 22);
 
   expect(manager.getRole()).toBe("Manager");
 });
