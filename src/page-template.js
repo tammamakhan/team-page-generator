3332162869831ffc8d61a9ddcc3bea9fb81cb29f
Tammam Khan <tammam.a.khan@gmail.com>
2022-06-01 21:56:22 -0700

    added function to generate HTML page

---
+++ src/page-template.js
@@ -0,0 +1,87 @@
+const generateManager = (manager) => {
+  return `<div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">${manager.getName()}</h5>
+    <p class="card-text">${manager.getRole()}</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">ID: ${manager.getId()}</li>
+    <li class="list-group-item">Email: ${manager.getEmail()}</li>
+    <li class="list-group-item">Office Number: ${manager.getOfficeNumber()}</li>
+  </ul>
+</div>`;
+};
+
+const generateEngineers = (teamMembers) => {
+  return `${teamMembers
+    .map((teamMember) => {
+      if (teamMember.getRole() !== "Engineer") return ``;
+
+      return `<div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">${teamMember.getName()}</h5>
+    <p class="card-text">${teamMember.getRole()}</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">ID: ${teamMember.getId()}</li>
+    <li class="list-group-item">Email: ${teamMember.getEmail()}</li>
+    <li class="list-group-item">Github: <a href="https://github.com/${teamMember.getGithub()}" class="card-link">${teamMember.getGithub()}</a></li>
+  </ul>
+  <div class="card-body">
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>`;
+    })
+    .join("")}
+`;
+};
+
+const generateInterns = (teamMembers) => {
+  return `${teamMembers
+    .map((teamMember) => {
+      if (teamMember.getRole() !== "Intern") return ``;
+
+      return `<div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">${teamMember.getName()}</h5>
+    <p class="card-text">${teamMember.getRole()}</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">ID: ${teamMember.getId()}</li>
+    <li class="list-group-item">Email: ${teamMember.getEmail()}</li>
+    <li class="list-group-item">School: ${teamMember.getSchool()}</li>
+  </ul>
+  <div class="card-body">
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>`;
+    })
+    .join("")}
+`;
+};
+
+const generatePage = (teamData) => {
+  return `<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>My Team</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container-fluid">
+      <h1 class="text-center font-weight-bold text-primary">My Team</h1>
+    </div>
+    <div class="container-fluid">
+      ${generateManager(teamData.manager)}
+      ${generateEngineers(teamData.teamMembers)}
+      ${generateInterns(teamData.teamMembers)}
+    </div>
+  </body>
+</html>`;
+};
+
+module.exports = generatePage;
